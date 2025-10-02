c62045e1e92eaaa79285ed3d61e139160dc46646
tirimid <tirimid@tirimid.net>
2025-10-02 15:05:19 +0400

    implementing map r/w

--- src/m_map.c
+++ src/m_map.c
@@ -1,5 +1,89 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+m_map_t m_map;
+
+i32
+m_read(FILE *fp)
+{
+	char magic[9] = {0};
+	if (fread(magic, 1, 8, fp) != 8)
+	{
+		z_err("map: failed to read 8xu8 magic!");
+		return 1;
+	}
+	
+	if (strcmp(magic, M_MAGIC))
+	{
+		z_err("map: invalid magic - %s!", magic);
+		return 1;
+	}
+	
+	u8 version;
+	if (fread(&version, 1, 1, fp) != 1)
+	{
+		z_err("map: failed to read 1xu8 version!");
+		return 1;
+	}
+	
+	if (version != M_VERSION)
+	{
+		z_err("map: unsupported version - %u!", version);
+		return 1;
+	}
+	
+	u16 w;
+	if (fread(&w, 2, 1, fp) != 1)
+	{
+		z_err("map: failed to read 1xu16 width!");
+		return 1;
+	}
+	
+	u16 h;
+	if (fread(&h, 2, 1, fp) != 1)
+	{
+		z_err("map: failed to read 1xu16 height!");
+		return 1;
+	}
+	
+	if (!w || !h)
+	{
+		z_err("map: invalid wxh - %ux%u!", w, h);
+		return 1;
+	}
+	
+	u16 nregs;
+	if (fread(&nregs, 2, 1, fp) != 1)
+	{
+		z_err("map: failed to read 1xu16 region count!");
+		return 1;
+	}
+	
+	u16 nattrs;
+	if (fread(&nattrs, 2, 1, fp) != 1)
+	{
+		z_err("map: failed to read 1xu16 attribute count!");
+		return 1;
+	}
+	
+	u16 nattrlists;
+	if (fread(&nattrlists, 2, 1, fp) != 1)
+	{
+		z_err("map: failed to read 1xu16 attribute list count!");
+		return 1;
+	}
+	
+	// TODO: read dynamically sized data.
+	
+	return 0;
+}
+
+i32
+m_write(FILE *fp)
+{
+	// TODO: implement map write to file.
+	return 1;
+}
+
 void
 m_render(void)
 {
--- src/m_map.h
+++ src/m_map.h
@@ -1,9 +1,103 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#define M_MAGIC "[2de]MAG"
+#define M_VERSION 1
+
+#define M_MAXWIDTH 65535
+#define M_MAXHEIGHT 65535
+#define M_MAXREGIONS 65535
+#define M_MAXATTRS 65535
+#define M_MAXATTRLISTS 65535
+
+// map file structure; start to end, data is packed.
+//
+// size (bytes)  field
+//
+// 8             magic string
+// 1             version
+//
+// 2             map width in tiles
+// 2             map height in tiles
+// 2             number of regions
+// 2             number of attributes
+// 2             number of attribute lists
+//
+// T             tile types
+// 2 * T         tile attribute lists
+//
+// R             region types
+// 4 * R         region x values
+// 4 * R         region y values
+// 4 * R         region w values
+// 4 * R         region h values
+// 2 * R         region attribute lists
+//
+// A             attribute types
+// 16 * A        attribute names
+// 16 * A        attribute values
+//
+// L             attribute list lengths
+// 32 * L        attribute lists
+//
+// note:
+// * T = map width * map height
+// * R = number of regions
+// * A = number of attributes
+// * L = number of attribute lists
+
+typedef enum m_attrtype
+{
+	M_INT = 0,
+	M_FLOAT,
+	M_STR,
+	M_BOOL
+} m_attrtype_t;
+
+typedef struct m_attrname
+{
+	char name[16];
+} m_attrname_t;
+
+typedef union m_attrval
+{
+	i64 i;
+	f64 f;
+	char s[16];
+	bool b;
+} m_attrval_t;
+
+typedef struct m_attrlist
+{
+	u16 attrs[16];
+} m_attrlist_t;
+
 typedef struct m_map
 {
-	u8 *data;
-	u32 w, h;
+	u16 w, h;
+	u16 nregs;
+	u16 nattrs;
+	u16 nattrlists;
+	
+	void *buf;
+	
+	u8 *tiletypes;
+	u16 *tileattrlists;
+	
+	u8 *regtypes;
+	f32 *regxs, *regys;
+	f32 *regws, *reghs;
+	u16 *regattrlists;
+	
+	u8 *attrtypes;
+	m_attrname_t *attrnames;
+	m_attrval_t *attrvals;
+	
+	u8 *attrlistlens;
+	m_attrlist_t *attrlists;
 } m_map_t;
 
+extern m_map_t m_map;
+
+i32 m_read(FILE *fp);
+i32 m_write(FILE *fp);
 void m_render(void);
